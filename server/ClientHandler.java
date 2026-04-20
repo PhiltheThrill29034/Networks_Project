fdadf1cd1f168c99f295fa01d6a144459de9fc57
Alexandra <konomi.alexa@gmail.com>
2026-04-21 00:41:06 +0300

    fixed a mistake I made in ClientHandler

--- server/ClientHandler.java
+++ server/ClientHandler.java
@@ -53,6 +53,12 @@ public class ClientHandler implements Runnable {
                 case "LOGIN" -> response = handleLogin(parts);
                 case "REQUEST_AUCTION" -> response = getAuctionRequest(parts);
                 case "LOGOUT" -> response = handleLogout(parts);
+                case "GET_CURRENT_AUCTION" -> response = server.getCurrentAuctionResponse();
+                case "GET_AUCTION_DETAILS" -> response = server.getAuctionDetailsResponse();
+                case "PLACE_BID" -> {
+                if (parts.length < 3) throw new IllegalArgumentException("Missing bid data");
+                    response = server.processBid(parts[1], Double.parseDouble(parts[2]));
+                }
             }
         } catch (IllegalArgumentException e){
             response = e.getMessage();
@@ -161,18 +167,5 @@ public class ClientHandler implements Runnable {
 
         server.removePeer(tokenId);
         return "LOGOUT_OK|Logged out successfully";
-    }
-
-    switch (parts[0]) {
-    case "REGISTER" -> response = handleRegister(parts);
-    case "LOGIN" -> response = handleLogin(parts);
-    case "REQUEST_AUCTION" -> response = getAuctionRequest(parts);
-    case "LOGOUT" -> response = handleLogout(parts);
-    case "GET_CURRENT_AUCTION" -> response = server.getCurrentAuctionResponse();
-    case "GET_AUCTION_DETAILS" -> response = server.getAuctionDetailsResponse();
-    case "PLACE_BID" -> {
-        if (parts.length < 3) throw new IllegalArgumentException("Missing bid data");
-        response = server.processBid(parts[1], Double.parseDouble(parts[2]));
-    }
     }
 }
